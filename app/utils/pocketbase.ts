65b3e52b5169af6002c3deef7d0035f8ebeba3b8
shum <shumkwunho@gmail.com>
2024-12-27 17:39:45 +0800

    push again

--- app/utils/pocketbase.ts
+++ app/utils/pocketbase.ts
@@ -1,14 +1,25 @@
 import PocketBase, { RecordModel } from "pocketbase"
 const pocketBaseIP = 'http://127.0.0.1:8090'
-import dotenv from 'dotenv'
 
-dotenv.config()
 
 class PbClient extends PocketBase {
 
 
     public constructor() {
         super(pocketBaseIP)
+
+    }
+
+
+    public async getTechStack() {
+        const techMap = await this.collection("tech_stack").getFullList({
+            fields: "id,name"
+        })
+
+        return techMap.reduce((acc, elem) => {
+            acc[elem.id] = elem.name;
+            return acc;
+        }, {} as Record<string, string>)
     }
 
 
@@ -31,7 +42,7 @@ class PbClient extends PocketBase {
             const res = await fetch(`${pocketBaseIP}/api/collections/projects/records/${id}?expand=feature_via_project_id,techStack`)
             const data = await res.json()
             return data
-
+            
         } catch (error) {
             console.log(error);
 
