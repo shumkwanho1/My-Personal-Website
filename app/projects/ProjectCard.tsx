94fa5ceba6c469683da7c9d1f2f2b8542513ff2b
shum <shumkwunho@gmail.com>
2025-01-01 23:00:56 +0800

    small fix

--- app/projects/ProjectCard.tsx
+++ app/projects/ProjectCard.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image"
 import Link from "next/link"
 import { projectType } from "../utils/type"
-import { showRandomPhoto } from "../utils/showRandomPhoto"
 
 
 type projectCardProps = {
